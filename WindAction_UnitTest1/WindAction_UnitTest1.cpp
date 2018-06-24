a62583ef212247d2221452ba76b3b372da69c1f0
Tiaan Krieg <worsman69@gmail.com>
2018-06-24 22:50:26 +0200

    Added Default testCase
    Signed-off-by: Tiaan Krieg <worsman69@gmail.com>

--- WindAction_UnitTest1/WindAction_UnitTest1.cpp
+++ WindAction_UnitTest1/WindAction_UnitTest1.cpp
@@ -13,6 +13,8 @@ namespace WindAction_UnitTest1
 		TEST_METHOD(TestMethod1)
 		{
 			// TODO: Your test code here
+      Assert::AreEqual(WindAction_library::WindAction::PeakWindSpeedPressure(1500, 28, 0.02, 12, 'A', 1.0), 929.47437481210955);
+
 		}
 
 	};
